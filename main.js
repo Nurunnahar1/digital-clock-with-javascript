1d3cae50a16949285c51ff97ac60670aa9b9d01f
Nurunnahar <nurunnahar9081@gmail.com>
2024-04-17 20:58:17 +0600

    stop watch

--- main.js
+++ main.js
@@ -5,21 +5,39 @@ const notification = document.querySelector("#notification");
 const alarm = document.querySelector("#alarm");
 
 let kaj;
-let count = 0;
+
+// let count = 0;
+// it will stop when it passes the specified time
+// startButton.onclick = (e) => {
+//   kaj = setInterval(() => {
+//       heading.innerHTML = count + 1;
+//       notification.play();
+
+//     if (count >= 5) {
+//         heading.innerHTML = "0";
+//         clearInterval(kaj);
+//         alarm.play();
+//     }
+
+//     count++;
+//   }, 1000);
+// };
 
 
+//   stop  watch
+let count = 10;
 startButton.onclick = (e) => {
   kaj = setInterval(() => {
-      heading.innerHTML = count + 1;
+      heading.innerHTML = count;
       notification.play();
 
-    if (count >= 5) {
+    if (count <= 0) {
         heading.innerHTML = "0"; 
         clearInterval(kaj);
         alarm.play();
     }
 
-    count++;
+    count--;
   }, 1000);
 };
 

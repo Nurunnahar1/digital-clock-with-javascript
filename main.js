602e8f86730f8772928059fc665b5ad5c469f716
Nurunnahar <nurunnahar9081@gmail.com>
2024-04-18 20:02:27 +0600

    Digital clock done

--- main.js
+++ main.js
@@ -1,49 +1,16 @@
-const startButton = document.querySelector(".box .start");
-const stopButton = document.querySelector(".box .stop");
 const heading = document.querySelector(".box h1");
 const notification = document.querySelector("#notification");
-const alarm = document.querySelector("#alarm");
 
-let kaj;
-
-// let count = 0;
-// it will stop when it passes the specified time
-// startButton.onclick = (e) => {
-//   kaj = setInterval(() => {
-//       heading.innerHTML = count + 1;
-//       notification.play();
-
-//     if (count >= 5) {
-//         heading.innerHTML = "0";
-//         clearInterval(kaj);
-//         alarm.play();
-//     }
-
-//     count++;
-//   }, 1000);
-// };
-
-
-//   stop  watch
-let count = 10;
-startButton.onclick = (e) => {
-  kaj = setInterval(() => {
-      heading.innerHTML = count;
-      notification.play();
-
-    if (count <= 0) {
-        heading.innerHTML = "0"; 
-        clearInterval(kaj);
-        alarm.play();
-    }
-
-    count--;
-  }, 1000);
+const digitalClockInit = () => {
+  const date = new Date();
+  heading.innerHTML = `
+    ${date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}:
+    ${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}:
+    ${date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()}`;
 };
+digitalClockInit();
 
-stopButton.onclick = () => {
-  clearInterval(kaj);
-    heading.innerHTML = "Stop.....";
-    notification.stop();
-    alarm.stop();
-};
+setInterval(() => {
+  digitalClockInit();
+  notification.play();
+}, 1000);
